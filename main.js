173fbcc87e45b5975faabdc0413c57e319e1f822
alex55k <123289675+alex55k@users.noreply.github.com>
2023-01-22 14:23:42 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+let textarea = "";
+let input = "";
+function setElem(textareaid, inputid){
+  if(textareaid == null || textareaid == ""){
+    console.log("setElem() occured an error. Missing: textareaid argument.");
+  } else {
+    if(document.getElementById(textareaid)){
+      textarea = document.getElementById(textareaid);
+    }else{
+    console.log("setElem() occured an error. Element with the id '" + textareaid + "' was not found.")
+    }
+  }
+  if(inputid == null || inputid == ""){
+    console.log("setElem() occured an error. Missing: inputid argument.");
+  } else {
+    if(document.getElementById(inputid)){
+      textarea = document.getElementById(inputid);
+    }else{
+    console.log("setElem() occured an error. Element with the id '" + inputid + "' was not found.")
+    }
+  }
+}
+
+let chat = "";
+const line = (n) => '='.repeat(n);
+const gen = (t) => `${line(t.length)}\n${t}\n${line(t.length)}`;
+
+const clear = (el) => {
+  el.value = '';
+};
+
+setInterval(() => {
+  s.scrollTop = s.scrollHeight;
+},0)
